037d9ab50200896479238bd188794a566818128c
Rohit Sahoo <rohitsahoo112003@gmail.com>
2024-01-19 00:19:55 +0530

    adding both folders

---
+++ step2reactlearning/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)    
+  
+  const changeValue = ()=>{
+    setCounter(counter + 1)
+    console.log("clicked", counter)
+  } 
+  const removeValue = ()=>{
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h2>Counter : {counter}</h2>
+
+      <button onClick={changeValue}
+      >Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
