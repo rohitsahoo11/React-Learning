56c1a58818ec0c149f539a775f9259c89a3d75f9
Rohit Sahoo <77395879+rohitsahoo11@users.noreply.github.com>
2024-03-18 12:45:22 +0530

    Delete AI Saas Project 1 directory

--- AI Saas Project 1/Backend/src/routes/chat-routes.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-
-const charRoutes = Router()
-
-export default charRoutes
--- AI Saas Project 1/Backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import userRoutes from "./user-routes.js";
-import charRoutes from "./chat-routes.js";
-
-const appRouter = Router()
-appRouter.use("/user", userRoutes) //domain/api/v1/users
-appRouter.use("/chats", charRoutes) //domain/api/v1/chat
-
-export default appRouter
